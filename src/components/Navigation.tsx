3d71d98e5a2d41b3e3a0b5b3de5ab992f93ef9bf
YoungSuChae <codudtn369@gmail.com>
2024-05-28 01:45:47 -0400

    add Naviagation bar with Logout feature (error: cannot login - "failed to fetch")

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,24 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { logoutUser } from "../services/authService";
 
-// logout function (use Api) - call back funtion -> main content -> landing page
-// navigation bar
-//
-function Navigation() {
+const Navigation: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      setIsLoggedIn(false);
+      await logoutUser();
+      navigate("/"); // Updated method to navigate
+    } catch (error) {
+      console.error("Failed to log out:", error);
+    }
+  };
+
   return (
     <>
       <Navbar bg="light" data-bs-theme="light">
@@ -15,11 +28,14 @@ function Navigation() {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <button onClick={handleLogout} style={{ margin: "auto" }}>
+              Logout
+            </button>
           </Nav>
         </Container>
       </Navbar>
     </>
   );
-}
+};
 
 export default Navigation;
