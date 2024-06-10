578d3fe0c52189b97f920356afb838c55beea5ec
YoungSuChae <codudtn369@gmail.com>
2024-06-09 23:50:11 -0400

    add dropdown menu for the logout feature. the dropdown menu can be used for profile feature and more.

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,14 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../services/authService";
+import "./App.css";
 
 const Navigation: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -19,6 +24,9 @@ const Navigation: React.FC = () => {
     }
   };
 
+  // Inside your Navigation component
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <>
       <Navbar bg="light" data-bs-theme="light">
@@ -28,9 +36,37 @@ const Navigation: React.FC = () => {
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <button onClick={handleLogout} style={{ margin: "auto" }}>
-              Logout
-            </button>
+          </Nav>
+          <Nav>
+            <Dropdown
+              show={dropdownOpen}
+              onToggle={(isOpen) => setDropdownOpen(isOpen)}
+            >
+              <Dropdown.Toggle
+                as={Button}
+                id="dropdown"
+                className={`customToggleButton ${dropdownOpen ? "active" : ""}`} // Dynamically add 'active' class based on dropdown state
+              >
+                <img
+                  src="/profile logo.png"
+                  alt="Profile Logo"
+                  width="32"
+                  height="32"
+                  className="rounded-circle"
+                />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action 1</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Action 2</Dropdown.Item>
+                <Dropdown.Item
+                  style={{ color: "Red", fontWeight: "600" }}
+                  onClick={handleLogout}
+                >
+                  Sign out
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Nav>
         </Container>
       </Navbar>
